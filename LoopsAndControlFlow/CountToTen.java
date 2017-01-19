e35c1a93f44168cdd828f5c91de3e6930ca2e3ae
Don Murphy <donmurphycs@users.noreply.github.com>
2017-01-19 11:41:02 -0800

    Moved to LoopsAndControlFlow folder; cleaned up mid-class changes

--- LoopsAndControlFlow/CountToTen.java
+++ LoopsAndControlFlow/CountToTen.java
@@ -2,8 +2,7 @@
 
 public class CountToTen {
     public static void main(String[] args) {
-        int i = 0;
-        for (; i <= 10; i++) {
+        for (int i = 0; i <= 10; i++) {
             System.out.print(i + " ");
         }
         System.out.println(i);
