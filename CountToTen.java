f4d2ce05107252d80ff641e32ebb1504af47e6bf
Don Murphy <donmurphycs@users.noreply.github.com>
2017-01-19 11:39:40 -0800

    Uploaded CountToTen example

---
+++ CountToTen.java
@@ -0,0 +1,11 @@
+// CountToTen.java
+
+public class CountToTen {
+    public static void main(String[] args) {
+        int i = 0;
+        for (; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println(i);
+    }
+}
